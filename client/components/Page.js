98a1160f64d7078bd90784a92d2da8b9a733d473
VolksRat71 <nathanieljryan1994@gmail.com>
2021-03-09 18:35:36 -0600

    templating some Nav and header stuff

--- client/components/Page.js
+++ client/components/Page.js
@@ -1,8 +1,10 @@
 /* eslint react/prop-types: 0 */
+import Header from './Header';
 
 export default function Page({ children }) {
   return (
     <>
+      <Header />
       <h1>I am the page component</h1>
       {children}
     </>
