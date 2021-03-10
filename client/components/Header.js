00ec64ac05b1718d6962354027de7e6e3acb846e
VolksRat71 <nathanieljryan1994@gmail.com>
2021-03-09 20:08:33 -0600

    Logo base design, we are going to have to circle back on this one

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,11 +1,28 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 import Nav from './Nav';
+
+const Logo = styled.h1`
+  background: red;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
 
 export default function Header() {
   return (
     <header>
       <div className="bar">
-        <Link href="/">NOT SUPREME</Link>
+        <Logo>
+          <Link href="/">NOT SUPREME</Link>
+        </Logo>
         <Nav />
       </div>
       <div className="sub-bar">
